d11c21073564554596adc8a548a5f1578666a43c
Venkata Thumu <vthumu@datacolor.com>
2025-10-18 14:23:39 -0400

    done first

--- src/stage/camera.ts
+++ src/stage/camera.ts
@@ -8,6 +8,7 @@ class CameraUniforms {
 
     set viewProjMat(mat: Float32Array) {
         // TODO-1.1: set the first 16 elements of `this.floatView` to the input `mat`
+        this.floatView.set(mat, 0);
     }
 
     // TODO-2: add extra functions to set values needed for light clustering here
@@ -38,6 +39,12 @@ export class Camera {
         // check `lights.ts` for examples of using `device.createBuffer()`
         //
         // note that you can add more variables (e.g. inverse proj matrix) to this buffer in later parts of the assignment
+
+        this.uniformsBuffer = device.createBuffer({
+            label: "camera",
+            size: this.uniforms.buffer.byteLength,
+            usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+        });
 
         this.projMat = mat4.perspective(toRadians(fovYDegrees), aspectRatio, Camera.nearPlane, Camera.farPlane);
 
@@ -130,9 +137,14 @@ export class Camera {
         const viewProjMat = mat4.mul(this.projMat, viewMat);
         // TODO-1.1: set `this.uniforms.viewProjMat` to the newly calculated view proj mat
 
+        this.uniforms.viewProjMat = viewProjMat;
+
         // TODO-2: write to extra buffers needed for light clustering here
 
         // TODO-1.1: upload `this.uniforms.buffer` (host side) to `this.uniformsBuffer` (device side)
         // check `lights.ts` for examples of using `device.queue.writeBuffer()`
+
+        device.queue.writeBuffer(this.uniformsBuffer, 0, this.uniforms.buffer);
+
     }
 }
